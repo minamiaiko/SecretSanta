0c3661365dc6bfdd5c0be7badf5b3f6ca28a7f44
Minami Tanaka <108184301+minamiaiko@users.noreply.github.com>
2023-03-08 20:44:40 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-###############################################################################
+###################################################################################################################################
 # SMTP configuration settings.
-###############################################################################
+###################################################################################################################################
 smtp = {
     'username': 'username',
     'password': 'password',
@@ -9,11 +9,11 @@ smtp = {
     'from_email': 'email-used-to-send-letters@examplecompany.gov',
 }
 
-###############################################################################
+###################################################################################################################################
 # This is the secret letter template that is used to send everyone the email.
 # Note that {santa} and {recipient} are automaticly replaced by the secret
 # Santa's name, and his/her recipient of their gift.
-###############################################################################
+###################################################################################################################################
 email_template = {
     'Bailey': 'bailey@examplecompany.gov',
     'Nelson': 'nelson@examplecompany.gov',
@@ -25,12 +25,10 @@ email_template = {
     'Kobe': 'kobe@examplecompany.gov',
 }
 
-###############################################################################
-# This contains a dictionary lookup of santa's (keys) who are not allowed to
-# have particular recipients (values).
-#
+###################################################################################################################################
+# This contains a dictionary lookup of santa's (keys) who are not allowed to have particular recipients (values).
 # If there are no incompatibles, leave this dictionary empty.
-###############################################################################
+###################################################################################################################################
 incompatibles = {
     # Do not allow Bailey to be a santa for Nelson
     'Bailey': ('Nelson',),
@@ -45,10 +43,7 @@ incompatibles = {
 #   'Bailey': ('Nelson', 'Minami', 'Jennifer', 'Lopez', 'Will', 'Smith', 'Kobe'),
 }
 
-###############################################################################
-# DON'T PEAK INTO THIS FILE!
-#
-# This file will contain a record of what was emailed. It will reveal who is
-# everyone's secret santa.
-###############################################################################
+###################################################################################################################################
+# DON'T PEAK INTO THIS FILE! This file will contain a record of what was emailed. It will reveal who is everyone's secret santa.
+###################################################################################################################################
 secret_santa_record_file = 'secret-santa-record=file.txt'
